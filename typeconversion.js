a909810160611567a92263c4867a321d47d03cb8
MunimUsmani <abdulmunimusmani152@gmail.com>
2024-03-23 22:50:30 +0500

    JavaScript Practice

---
+++ typeconversion.js
@@ -0,0 +1,17 @@
+// let age;
+// age = window.prompt("what's your age?");
+// console.log(typeof age);
+// age = Number(age);
+// age += 1;
+// console.log("happy bday you are now",age,"years old");
+
+
+let x;
+let y;
+
+x = Number(3.21);
+y = String(3.35)
+console.log(x, typeof x);
+console.log(y, typeof y);
+
+// if you convert a number in string itll return NaN
