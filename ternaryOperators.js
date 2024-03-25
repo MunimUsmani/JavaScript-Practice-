aab8dc3e097f6e0b34e2843c354dae903fbee683
MunimUsmani <abdulmunimusmani152@gmail.com>
2024-03-25 22:54:26 +0500

    JavaScript Practice

---
+++ ternaryOperators.js
@@ -0,0 +1,14 @@
+
+// let time = 17;
+// let greeting = time < 12 ? "Good Morning" : "Good Afternoon";
+// console.log(greeting);
+
+
+
+
+
+ let amount = 125;
+
+ let discount = amount >= 100 ? 10 : 0;
+
+ console.log(amount - amount * (discount/100));
