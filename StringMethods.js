db1a9be80160375634daa0e90105850d9138574a
MunimUsmani <abdulmunimusmani152@gmail.com>
2024-03-26 20:08:06 +0500

    JavaScript Practice

---
+++ StringMethods.js
@@ -0,0 +1,34 @@
+
+let username = "munim";
+ let result = username.startsWith("  ")
+ let results = username.endsWith("  ")
+ let resultss = username.includes("")
+
+ console.log(result);
+ console.log(results);
+ console.log(resultss);
+
+
+
+  console.log(username.charAt(1));
+console.log(username.indexOf("u"));
+   console.log(username.lastIndexOf("u"));
+   console.log(username.length);
+
+   username = username.trim();
+  username = username.toUpperCase();
+   username = username.toLowerCase();
+   username = username.repeat(3);
+  console.log(username);
+
+//  let phoneNumber = 123-456-789;
+//  phoneNumber = phoneNumber.replace("-","");
+//  console.log(phoneNumber);
+
+//   let phoneNumber = 123-456-789;
+//   phoneNumber = phoneNumber.padStart(15,"0");
+//   console.log(phoneNumber);
+
+//   let phoneNumbers = 123-456-789;
+//   phoneNumbers = phoneNumbers.padEnd(15,"0");
+//   console.log(phoneNumber);
