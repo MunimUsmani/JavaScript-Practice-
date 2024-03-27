0b3d270cd8f4319e5173bc16518cb546fef53b68
MunimUsmani <abdulmunimusmani152@gmail.com>
2024-03-27 23:28:34 +0500

    JavaScript Practice

---
+++ LogicalOperators.js
@@ -0,0 +1,25 @@
+
+// let temp = 20;
+
+// if (temp > 0 && temp <= 30) {
+//     console.log("the weather is good");
+// }
+// else{
+//     console.log("the weather is bad");
+// }
+
+// let temp = 26;
+
+// if (temp <=0 || temp > 35) {
+//     console.log("the weather is bad");
+// }
+// else{
+//     console.log("the weather is good");
+// }
+
+const isSunny = true;
+if (!isSunny) {
+    console.log("it is raining");
+} else {
+    console.log("it is sunny");
+}
