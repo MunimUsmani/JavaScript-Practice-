2aca9035e69c95420b176c667ffb5c7d29f75c29
MunimUsmani <abdulmunimusmani152@gmail.com>
2024-03-28 22:49:41 +0500

    JavaScript Practice

---
+++ strictequality.js
@@ -0,0 +1,18 @@
+// = assignment operator, == check if two values are equal, === check if two values and there data types are equal, != inequality operator, !== strict equality operator
+
+
+const pi = 3.14;
+
+if (pi !== "3.14") {
+    console.log("that is pi");
+} else {
+    console.log("that is not pi");
+}
+
+// const pi = 3.14;
+
+// if (pi != "3.14") {
+//     console.log("that is pi");
+// } else {
+//     console.log("that is not pi");
+// }
