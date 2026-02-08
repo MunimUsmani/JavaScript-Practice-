9236ef01a83ebda1735928879cdff2f13a6be5ee
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2026-02-08 10:16:03 +0500

    Add asynchronous task execution with promises

---
+++ asyncawait.js
@@ -0,0 +1,34 @@
+function task1() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log("Task 1 done");
+      resolve();
+    }, 1000);
+  });
+}
+
+function task2() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log("Task 2 done");
+      resolve();
+    }, 1000);
+  });
+}
+
+function task3() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log("Task 3 done");
+      resolve();
+    }, 1000);
+  });
+}
+
+async function executeTasks() {
+  await task1();
+  await task2();
+  await task3();
+  console.log("All tasks completed");
+}
+executeTasks();
